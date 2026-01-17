4ad60c21c1bdb54eace4422e80ddd9fcc89f1871
mellurboo <mellurblur@gmail.com>
2026-01-17 03:21:00 +0000

    optional relative pathing for programs

--- src/commands/impl/spawn.c
+++ src/commands/impl/spawn.c
@@ -2,12 +2,39 @@
 #include <stdio.h>
 #include <syscalls/spawn.h>
 #include <syscalls/waitpid.h>
+#include <syscalls/getcwd.h>
 #include <commands/commands.h>
+#include <string.h>
+#include <limits.h>
 
 int cmd_spawn(shell_cmd_t *cmd){
-    (void)cmd;
-    
-    int pid = _spawn(cmd->argv[2]);
+    if (cmd->argc < 2){
+        printf("Usage: spawn <program>\n");
+        return -1;
+    }
+
+    const char *prog = cmd->argv[1];
+    char resolved[PATH_MAX];
+
+    if (prog[0] == '/') {
+        strncpy(resolved, prog, PATH_MAX - 1);
+        resolved[PATH_MAX - 1] = '\0';
+    } else {
+        char cwd[PATH_MAX] = {0};
+        if (!_getcwd(cwd, PATH_MAX)) {
+            printf("spawn: cannot get current directory\n");
+            return -1;
+        }
+
+        size_t cwd_len = strlen(cwd);
+        if (cwd_len == 1 && cwd[0] == '/') {
+            snprintf(resolved, PATH_MAX, "/%s", prog);
+        } else {
+            snprintf(resolved, PATH_MAX, "%s/%s", cwd, prog);
+        }
+    }
+
+    int pid = _spawn(resolved);
     if (pid < 0){
         printf(LOG_ERROR "Kernel Attempted to start Program but got error code: %d\n", pid);
         return pid;
